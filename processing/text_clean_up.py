56d0195efad78e7b80adeb6efc1aa412b62f2421
mira <miraslamaj@gmail.com>
2026-06-17 21:28:04 +0300

    camel case

--- processing/text_clean_up.py
+++ processing/text_clean_up.py
@@ -2,18 +2,39 @@
 This file's purpose is about the required text pre-processing step.
 
 It includes the following:
-    1. Convert to lowercase representation
-    2. Normalize hashtags (remove '#', '_' and separate words)
-    3. Remove punctuation
-    4. Remove stopwords
-    5. Remove unnecessary information (like eg "subscribe") -> only for linked_article_text
-    6. Lemmatisation
+    1. Split came case representation 
+    2. Convert to lowercase representation
+    3. Replace 'ς' with 'σ'
+    4. Remove '#', '_' (for hashtags and word_segmentation)
+    5. Remove accents
+    6. Remove punctuation
+    7. Remove stopwords
+    8. Remove unnecessary information (like eg "subscribe") -> only for linked_article_text
+    9. Lemmatisation
 
 """
+import re
+import unicodedata
 
 def normalize(text):
+
+    text=split_camel_case(text)
+
     text=text.lower()
 
     text=text.replace('#','')
     text=text.replace('_',' ') #add whitespace for '_'
+    text=text.replace('ς','σ')
+
+    #remove accents (greek tonal system)
+    text = ''.join(
+        c for c in unicodedata.normalize('NFD', text)
+        if unicodedata.category(c) != 'Mn'
+    )
+
     return text
+
+
+def split_camel_case(text):
+    #E.G. for 'NoIdea' this function returns 'No Idea'
+    return re.sub(r'(?<=[a-zα-ω])(?=[A-ZΑ-Ω])', ' ', text)
